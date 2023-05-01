731b5e3f9536daec494be79a5380279922079ed3
rhit-cerasoml <cerasoml@rose-hulman.edu>
2023-04-30 22:15:29 -0400

    Added player attacks + minor fixes

--- Assets/Scripts/EnemyHitPlayer.cs
+++ Assets/Scripts/EnemyHitPlayer.cs
@@ -2,18 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyHitPlayer : MonoBehaviour
-{
-
-    void OnCollisionEnter2D(Collision2D collision)
-    {
-        Debug.Log("onCollsion");
+public class EnemyHitPlayer : MonoBehaviour {
+    void OnCollisionEnter2D(Collision2D collision) {
         GameObject other = collision.gameObject;
-        if (GameManager.Instance.GetPlayer() == other)
-        {
+        if (GameManager.Instance.GetPlayer() == other) {
             // HERE we know that the other object we collided with is an enemy
             GameManager.Instance.DecrementScoreCount();
         }
-    }
-    
+    }   
 }
--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -6,18 +6,22 @@ using UnityEngine;
 
 public class PlayerInput : MonoBehaviour {
 
+    // CONFIGS
     public float speed = 4.5f;
     public float jumpForce = 5.0f;
     public int JumpCount = 2;
     private int JumpsLeft = 0;
     public ForceMode2D jumpMode = ForceMode2D.Impulse;
 
+    // CAMERA
     [SerializeField] Camera _camera;
     [SerializeField] float smoothLerpFactor;
 
+    // PHYSICS
     private Rigidbody2D body;
     private CircleCollider2D _collider;
 
+    // SOUND
     private AudioSource _audio_source;
     private bool _grounded = false;
     private bool _is_walking = false;
@@ -27,12 +31,18 @@ public class PlayerInput : MonoBehaviour {
     [SerializeField] AudioClip sound_jump;
     [SerializeField] AudioClip sound_walk;
 
+    // ATTACK
+    private GameObject _attack_collider;
+    private bool _attack_on_cooldown = false;
+    [SerializeField] float attack_cooldown;
+
     // Start is called before the first frame update
     void Start() {
         _camera = GameManager.Instance.getPlayerCamera();
         body = GetComponent<Rigidbody2D>();
         _collider = GetComponent<CircleCollider2D>();
         _audio_source = GetComponent<AudioSource>();
+        _attack_collider = transform.Find("AttackCollider").gameObject;
     }
 
     // Update is called once per frame
@@ -64,19 +74,30 @@ public class PlayerInput : MonoBehaviour {
 
         _is_walking = Mathf.Abs(deltaX) > 0.1f;
 
+        if(_is_walking){
+            float direction = Mathf.Sign(deltaX);
+            updateDirection(direction > 0);
+        }
+
         if(grounded && _is_walking && !_is_walk_playing){
             _is_walk_playing = true;
             StartCoroutine(WalkSound());
         }
 
-        if((Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.W)) && JumpsLeft > 0){
+        if((Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)) && JumpsLeft > 0){
             vel.y = 0;
             body.velocity = vel;
             body.AddForce(Vector2.up * jumpForce, jumpMode);
             JumpsLeft--;
             _audio_source.PlayOneShot(sound_jump);
         }
+
+        if((Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow)) && !_attack_on_cooldown){
+            onAttack();
+        }
     }
+
+
 
     IEnumerator WalkSound() {
         while(_grounded && _is_walking){
@@ -86,6 +107,11 @@ public class PlayerInput : MonoBehaviour {
         _is_walk_playing = false;
     }
 
+    IEnumerator AttackCooldown(){
+        yield return new WaitForSeconds(attack_cooldown);
+        _attack_on_cooldown = false;
+    }
+
     void FixedUpdate(){
         Vector2 cameraFlatCoords = Vector2.Lerp(_camera.transform.position, transform.position, smoothLerpFactor);
         _camera.transform.position = new Vector3(
@@ -93,5 +119,39 @@ public class PlayerInput : MonoBehaviour {
                 cameraFlatCoords.y,
                 -10
             );
+    }
+
+    void updateDirection(bool right_facing){
+        if(right_facing){
+            _attack_collider.transform.position = new Vector3(transform.position.x + 0.75f, transform.position.y, transform.position.z);
+        }else{
+            _attack_collider.transform.position = new Vector3(transform.position.x - 0.75f, transform.position.y, transform.position.z);
+        }
+    }
+
+    void onAttack(){
+        _attack_on_cooldown = true;
+        StartCoroutine(AttackCooldown());
+        Debug.Log("ATTACK!");
+
+        ContactFilter2D filter = new ContactFilter2D();
+        filter.NoFilter();
+        List<Collider2D> overlapColliders = new List<Collider2D>();
+
+        _attack_collider.SetActive(true);
+        int count = _attack_collider.GetComponent<BoxCollider2D>().OverlapCollider(filter, overlapColliders);
+        Debug.Log(count);
+        if(count != 0){
+            foreach(Collider2D collider in overlapColliders){
+                Debug.Log("HIT: " + collider);
+                GameObject gameObject = collider.gameObject;
+                if(gameObject.tag == "Enemy"){
+                    Destroy(gameObject);
+                    GameManager.Instance.IncrementScoreCount();
+                }
+            }
+        }
+        _attack_collider.SetActive(false);
+
     }
 }
